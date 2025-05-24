9ed8356d29a2a415b6bd856546d7de6d713bcc18
Labib BENALI <labib9774@gmail.com>
2025-05-24 18:19:55 +0200

    first import

---
+++ src/main/java/keycloak/integration/spring/KeycloakIntegrationSpringApplication.java
@@ -0,0 +1,13 @@
+package keycloak.integration.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakIntegrationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakIntegrationSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/keycloak/integration/spring/KeycloakIntegrationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package keycloak.integration.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakIntegrationSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
